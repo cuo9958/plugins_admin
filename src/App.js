3dc83f43860b0e42d2ba83937c9b05d611a09540
guofangchao <guofangchao@daling.com>
2018-11-06 20:40:02 +0800

    add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Button } from 'antd';
+import { Layout, Button } from 'antd';
+import NProcess from "nprogress"
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <Button type="primary">ceshi anz</Button>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return <Layout id="app">
+            <div className="header">
+                <h4>header</h4>
+            </div>
+            <div className="main">
+                <div className="sub">
+                    <h4>sub</h4>
+                </div>
+                <div className="main-inner">
+                    <Button onClick={this.test1.bind(this)} type="primary">ceshi anz</Button>
+                    <Button onClick={this.test2.bind(this)} type="primary">ceshi anz</Button>
+                    <div className="footer">
+                        <h4>footer</h4>
+                    </div>
+                </div>
+
+            </div>
+
+        </Layout>
+    }
+    test1() {
+        NProcess.start();
+    }
+    test2() {
+        NProcess.set(0.4);
+        setTimeout(() => {
+            NProcess.done();
+
+        }, 1000);
+    }
 }
 
 export default App;
